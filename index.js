2f6097382d94e6169e8a86fae2594033ed0e10c3
Vaibhav Lohani <lohanivaibhav4@gmail.com>
2024-11-06 15:17:58 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,30 @@
+let hScore = document.getElementById("hscore")
+let hs = 0;
+function oneplus(){
+    hs++;
+    hScore.textContent =hs;
+}
+function twoplus(){
+    hs +=2;
+    hScore.textContent =hs;
+}
+function threeplus(){
+    hs +=3;
+    hScore.textContent =hs;
+}
+
+let gScore = document.getElementById("gscore")
+let gs = 0;
+function goneplus(){
+    gs++;
+    gScore.textContent =gs;
+}
+function gtwoplus(){
+    gs +=2;
+    gScore.textContent =gs;
+}
+function gthreeplus(){
+    gs +=3;
+    gScore.textContent =gs;
+}
+
